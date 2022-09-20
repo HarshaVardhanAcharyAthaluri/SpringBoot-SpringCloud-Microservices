8159a8aaadf70a897b0505797dfaa6ada510bb48
HarshaVardhan <athaluri.harsha@gmail.com>
2022-09-20 09:05:59 +0530

    Spring Security added

---
+++ SpringSecurityBasicAuthenticationINmemory/springsecurity/src/main/java/com/training/springsecurity/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.training.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+	public void addViewControllers(ViewControllerRegistry reg) {
+		reg.addViewController("/greet").setViewName("greet");
+		reg.addViewController("/home").setViewName("home");
+		reg.addViewController("/").setViewName("home");
+	}
+	
+}
---
+++ SpringSecurityBasicAuthenticationINmemory/springsecurity/src/main/java/com/training/springsecurity/config/WebSecurity.java
@@ -0,0 +1,38 @@
+package com.training.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		.antMatchers("/home","/").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().permitAll()
+		.and()
+		.logout().permitAll();
+		
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails userdeatils = User.withDefaultPasswordEncoder()
+				.username("harsha")
+				.password("123456")
+				.roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(userdeatils);
+	}
+	
+}
